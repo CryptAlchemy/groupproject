b269c75bb086c4a3be8cf4e0226a1eee57f3b9a2
CryptAlchemy <CryptAlchemy@users.noreply.github.com>
2020-12-14 23:23:10 -0500

    Final

--- public_html/profile.php
+++ public_html/profile.php
@@ -8,9 +8,9 @@ if(!isset($_SESSION['uid'])) {
 ?>
 <body>
 	<div class="container mt-5 d-flex justify-content-center">
-		<div class="d-flex bg-light p-3 rounded">
+		<div class="d-flex bg-light p-3 rounded" class="border border-primary"></span>
 			<div class="image">
-				<img src="/assets/img/pfp/<?=$UserInfo['pfp']?>.png"  class="img-responsive" class="rounded" width="155">
+				<img src="/assets/img/pfp/<?=$UserInfo['pfp']?>.png"  class="img-re	sponsive" class="rounded" width="155">
 			</div>
 			<div class="ml-3">
 				<p class="font-weight-bold h4"><?=$UserInfo['username'] ?></i></p>
